ecdf3947b6926af9563a4dd22be2918e09e4a178
Samat22 <57350400+Samat22@users.noreply.github.com>
2019-11-12 11:09:19 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Hero s = new Hero(150, 30, 25);
         System.out.println(s.heroHealths + " " + s.heroAttacks + " " + s.heroAttackTypes);
+        Boss boss = new Boss();
+        boss.setAttack(40);
+        boss.setHealth(700);
+        boss.setDefenceType(50);
+        System.out.println(boss.getAttack()+ " "+boss.getHealth()+" "+ boss.getDefenceType());
     }
 
-    Boss boss = new Boss();
+    
 
     // write your code here
 }
